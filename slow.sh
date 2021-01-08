502f3cd4a06fdc876bf9520e40c4ffcd4c0d7eb7
batyarimskiy <70210154+batyarimskiy@users.noreply.github.com>
2021-01-09 01:05:01 +0300

    Delete slow.sh

--- slow.sh
+++
@@ -1,9 +0,0 @@
-apt install python3
-apt install nmap
-apt install hydra
-pip install colored
-pip install requests
-mv fire /data/data/com.termux/files/usr/bin
-clear
-echo 'Установка прошла успешно!'
-rm slow.sh
