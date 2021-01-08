cbfdf4926cd446571166189a01e765b66220d713
batyarimskiy <70210154+batyarimskiy@users.noreply.github.com>
2021-01-09 01:05:43 +0300

    Add files via upload

---
+++ slow.sh
@@ -0,0 +1,12 @@
+apt install python3
+apt install nmap
+apt install hydra
+pip install colored
+pip install requests
+cd scripts
+unzip weber.zip
+cd ..
+mv fire /data/data/com.termux/files/usr/bin
+clear
+echo 'Установка прошла успешно!'
+rm slow.sh
